3149587b53c599fa03c9b25b2015eb2bcf982491
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2020-10-07 13:41:59 -0400

    Completed Lab 2

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Switch } from 'react-native';
 
 export default function App() {
+
+  const [value, onChangeText] = useState('Placeholder');
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View></View>
+    <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+      <TextInput
+        style = {styles.edit}
+        clearTextOnFocus
+        onChangeText = {text => onChangeText(text)}
+        value = {value}
+      />
+      <Text>{value}</Text>
+      <Button
+        style = {styles.button}
+        title='clear'
+        color='red'
+        onPress={() => onChangeText('')}
+        />
     </View>
   );
 }
@@ -17,5 +39,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 100,
   },
+  edit: {
+    borderColor: 'black',
+    borderWidth: 1,
+    width: '90%',
+  },
+  text: {
+
+  },
+  button: {
+    backgroundColor: 'blue',
+    color: "white",
+  }
 });
